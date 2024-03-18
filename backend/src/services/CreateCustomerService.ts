a157eb8c8b545fce7c5c152db20d7141e3dde174
gabrielfel1x <gabrielfelxx@gmail.com>
2024-03-18 19:33:49 -0300

    finished api (GET)

--- backend/src/services/CreateCustomerService.ts
+++ backend/src/services/CreateCustomerService.ts
@@ -1,10 +1,27 @@
 import prismaClient from "../prisma";
 
+interface CreateCrustomerProps{
+    name: string;
+    email: string;
+}
+
 class CreateCustomerService {
-    async execute() {
+    async execute({ name, email }: CreateCrustomerProps) {
         console.log("the route was called");
 
-        return { ok: true }
+        if (!name || !email) {
+            throw new Error("not all fields are filled in!")
+        }
+
+        const costumer = await prismaClient.customer.create({
+            data: {
+                name,
+                email,
+                status: true
+            }
+        })
+
+        return costumer
     }
 }
 
