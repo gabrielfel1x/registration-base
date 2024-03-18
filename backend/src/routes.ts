1b1da3815a27a321b123d3c242882f4d6c260824
gabrielfel1x <gabrielfelxx@gmail.com>
2024-03-18 20:47:02 -0300

    add delete costumer route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,39 @@
-import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from 'fastify';
+import {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
-import { ListCostumerController } from "./controllers/ListCostumerController"
+import { ListCostumerController } from "./controllers/ListCostumerController";
+import { DeleteCostumerController } from "./controllers/DelCostumerController";
 
-export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions
+) {
+  fastify.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
+    return { ok: true };
+  });
 
-    fastify.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
-        return { ok: true }
-    })
+  fastify.post(
+    "/customer",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new CreateCustomerController().handle(request, reply);
+    }
+  );
 
-    fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
-        return new CreateCustomerController().handle(request, reply)
-    })
+  fastify.get(
+    "/customers",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new ListCostumerController().handle(request, reply);
+    }
+  );
 
-    fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply) => {
-        return new ListCostumerController().handle(request, reply)
-    })
+  fastify.delete(
+    "/customer",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new DeleteCostumerController().handle(request, reply);
+    }
+  );
 }
