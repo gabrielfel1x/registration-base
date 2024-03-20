bf15e507906927ed47b54fb4959f0daac5fe2d7a
gabrielfel1x <gabrielfelxx@gmail.com>
2024-03-20 13:04:48 -0300

    add developer credits

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1 class="font-bold text-5xl text-gray-900">CUSTOMERS</h1>
+    <span class="font-light">developed by <a href="github.com/gabrielfel1x" class="font-semibold text-gray-900">gabriel felix</a></span>
   </div>
 </template>
 
